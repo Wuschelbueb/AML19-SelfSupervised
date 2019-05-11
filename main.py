3e23fc6217bb50745ae97f91e809063e1e2cf4b8
Ramona <ramona.beck@bluewin.ch>
2019-05-11 16:10:25 +0200

    - Fix ExemplarCNN - Refactor Files

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-import torch
+"""Main"""
 
-from RotationModel import ResNet20, ResNet20ExemplarCNN, train_exemplar_cnn_model, train_rotation_model, fine_tune_fc, \
-    fine_tune_variant_2
+from rotation import train_rotation_net, fine_tune_rotation_model
+from exemplar_cnn import train_exemplar_cnn, fine_tune_exemplar_cnn
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+rotation_model_trained = train_rotation_net()
+classification_model_1 = fine_tune_rotation_model(rotation_model_trained, False, False, False, True)
 
-resnet20 = ResNet20()
-resnet20 = resnet20.to(device)
-
-
-resnet_20_exemplar_cnn = ResNet20ExemplarCNN()
-resnet_20_exemplar_cnn = resnet_20_exemplar_cnn.to(device)
-
-print("Train ResNet20 with ExemplarCNN\n")
-resnet_20_exemplar_cnn_trained = train_exemplar_cnn_model(resnet_20_exemplar_cnn)
-print("\n=========================================\n")
-
-
-# print("Train ResNet20\n")
-# resnet20_trained = train_rotation_model(resnet20)
-# print("\n=========================================\n")
-#
-# print("\nFinetune ResNet20 (only linear layer)\n")
-# resnet20_finetuned_1 = fine_tune_fc(resnet20_trained)
-# print("\n=========================================\n")
-#
-# print("\nFinetune ResNet20 (layer 3 + linear layer)\n")
-# resnet20_finetuned_2 = fine_tune_variant_2(resnet20_trained)
-# print("\n=========================================\n")
+exemplar_cnn_trained = train_exemplar_cnn()
+classification_model_2 = fine_tune_exemplar_cnn(exemplar_cnn_trained, False, False, False, True)
---
+++ rotation.py
@@ -0,0 +1,81 @@
+"""Handles the rotation sub task."""
+import torch
+import torch.nn as nn
+
+from train import train_and_val
+from resnet import ResNet20
+from fashion_mnist_data_handler import train_loader_classification, val_loader_classification, \
+    test_loader_classification, train_loader_rotation, \
+    val_loader_rotation, test_loader_rotation
+
+EPOCHS = 15
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_loader_classification = train_loader_classification()
+val_loader_classification = val_loader_classification()
+test_loader_classification = test_loader_classification()
+
+train_loader_rotation = train_loader_rotation()
+val_loader_rotation = val_loader_rotation()
+test_loader_rotation = test_loader_rotation()
+
+
+def train_rotation_net():
+    """Trains the rotation model."""
+    print("===========================================")
+    print("========== Train Rotation Model ===========")
+    print("===========================================\n")
+
+    rotation_model = ResNet20()
+    rotation_model = rotation_model.to(device)
+
+    # fitting the convolution to 1 input channel (instead of 3)
+    rotation_model.conv = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1, bias=False)
+
+    # Criteria NLLLoss which is recommended with Softmax final layer
+    loss_fn = nn.CrossEntropyLoss()
+
+    # Observe that all parameters are being optimized
+    optimizer = torch.optim.Adam(rotation_model.parameters(), lr=0.001)
+
+    # Decay LR by a factor of 0.1 every 4 epochs
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=4, gamma=0.1)
+
+    return train_and_val(rotation_model, loss_fn, optimizer, scheduler, EPOCHS,
+                         train_loader_rotation, val_loader_rotation)
+
+
+def fine_tune_rotation_model(model, unfreeze_l1, unfreeze_l2, unfreeze_l3, unfreeze_fc):
+    """Fine tunes the rotation model."""
+    print("===========================================")
+    print("======== Fine Tune Rotation Model =========")
+    print("===========================================\n")
+
+    # Criteria NLLLoss which is recommended with Softmax final layer
+    loss_fn = nn.CrossEntropyLoss()
+
+    # freezes the layers according to the method parameters
+    for param in model.layer1.parameters():
+        param.requires_grad = unfreeze_l1
+
+    for param in model.layer2.parameters():
+        param.requires_grad = unfreeze_l2
+
+    for param in model.layer3.parameters():
+        param.requires_grad = unfreeze_l3
+
+    for param in model.fc.parameters():
+        param.requires_grad = unfreeze_fc
+
+    # replace fc layer with 10 outputs
+    model.fc = nn.Linear(64, 10)
+
+    # Observe that all parameters are being optimized
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    # Decay LR by a factor of 0.1 every 4 epochs
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=4, gamma=0.1)
+
+    model = model.to(device)
+    return train_and_val(model, loss_fn, optimizer, scheduler, EPOCHS,
+                         train_loader_classification, val_loader_classification)
