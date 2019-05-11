250e49b470a616635e7aaccdb98ba76f004296e5
Ramona <ramona.beck@bluewin.ch>
2019-05-11 22:58:01 +0200

    Fix plot method

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@ Created on Tue May  7 14:49:27 2019
 
 @author: Gwenael
 """
-
+import os
 import numpy as np
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 
@@ -20,11 +22,26 @@ def plot_n_curves(values, legend, title="n curves graph", axis1="epochs", axis2=
         axis2= "loss/val"
     )
     """
-    plt.figure()
+    fig = plt.figure()
+
     for v in values:
         plt.plot(np.arange(len(v)), v)
     plt.legend(legend)
     plt.xlabel(axis1)
     plt.ylabel(axis2)
     plt.title(title)
-    plt.savefig(title + '.pdf')
+
+    split = title.split(' ')
+    s = '_'
+    concat_title = s.join(split)
+    final_title = concat_title + '.pdf'
+
+    # Pick one of the following lines to uncomment
+    # save_file = None
+    save_file = os.path.join('./', final_title)
+
+    if save_file:
+        plt.savefig(save_file)
+        plt.close(fig)
+    else:
+        plt.show()
