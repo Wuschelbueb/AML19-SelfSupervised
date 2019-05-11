c193676c88fa7bca8c5e6a5f707962516bde5757
Ramona <ramona.beck@bluewin.ch>
2019-05-11 17:15:51 +0200

    Refactor plot method and try some plots (no clue if it works, need to test on node)

--- main.py
+++ main.py
@@ -2,9 +2,20 @@
 
 from rotation import train_rotation_net, fine_tune_rotation_model
 from exemplar_cnn import train_exemplar_cnn, fine_tune_exemplar_cnn
+from utils import plot_n_curves
 
-rotation_model_trained = train_rotation_net()
-classification_model_1 = fine_tune_rotation_model(rotation_model_trained, False, False, False, True)
+rotation_trained, train_losses_rot, val_losses_rot, train_accuracies_rot, val_accuracies_rot = train_rotation_net()
+plot_n_curves([train_losses_rot, val_losses_rot], ["train loss", "val loss"], "Loss for rotation task")
+plot_n_curves([train_accuracies_rot, val_accuracies_rot], ["train accuracy", "val accuracy"],
+              "Accuracy for rotation task")
 
-exemplar_cnn_trained = train_exemplar_cnn()
-classification_model_2 = fine_tune_exemplar_cnn(exemplar_cnn_trained, False, False, False, True)
+# classification_model_1, train_losses, val_losses, train_accuracies, val_accuracies = fine_tune_rotation_model(
+#     rotation_trained, False, False, False, True)
+
+
+exemplar_cnn_trained, train_losses_ex, train_accuracies_ex = train_exemplar_cnn()
+plot_n_curves([train_losses_ex], ["train loss"], "Loss for ExemplarCNN")
+plot_n_curves([train_accuracies_ex], ["train accuracy"], "Accuracy for ExemplarCNN")
+
+# classification_model_2, train_losses_c, val_losses, train_accuracies, val_accuracies = fine_tune_exemplar_cnn(
+#     exemplar_cnn_trained, False, False, False, True)
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue May  7 14:49:27 2019
 
@@ -8,18 +7,24 @@ Created on Tue May  7 14:49:27 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_n_curves(values, legend,title="n curves graph",  axis1 = "epochs", axis2= "loss/val"):
+
+def plot_n_curves(values, legend, title="n curves graph", axis1="epochs", axis2="loss/val"):
     """
     Plot n curves in the same graph. use as follows
     
-    plot_n_curves([[1,2,3,4,5],[4,5,4,5,4],[2,2,2,2,2]], ["growing", "oscillating", "constant"], title = "3 different curves", axis1 = "epochs", axis2= "loss/val")    
+    plot_n_curves(
+        [[1,2,3,4,5],[4,5,4,5,4],[2,2,2,2,2]],
+        ["growing", "oscillating", "constant"],
+        title = "3 different curves",
+        axis1 = "epochs",
+        axis2= "loss/val"
+    )
     """
     plt.figure()
     for v in values:
-         plt.plot(np.arange(len(v)), v)
+        plt.plot(np.arange(len(v)), v)
     plt.legend(legend)
     plt.xlabel(axis1)
     plt.ylabel(axis2)
     plt.title(title)
-    
-    
+    plt.savefig(title + '.pdf')
