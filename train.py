de05744f6555791e0ad1ad523f4c5f54f7a0d0cb
Nathalie Froidevaux <nathalie.froidevaux@students.unibe.ch>
2019-05-12 16:39:36 +0200

    removed unused code

--- train.py
+++ train.py
@@ -69,11 +69,6 @@ def train_and_val(model, loss_fn, optimizer, scheduler, num_epochs, train_loader
         val_losses.append(np.mean(np.array(running_loss)))
         val_accuracies.append(100.0 * running_corrects_val / len(val_loader.dataset))
 
-        # deep copy the model
-        # if running_corrects_val > best_acc:
-        #     best_acc = running_corrects_val
-        #     best_model_wts = model.state_dict()
-
         if val_accuracies[-1] > best_acc:
             best_acc = val_accuracies[-1]
 
