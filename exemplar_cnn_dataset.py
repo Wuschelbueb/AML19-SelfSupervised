fb1a4267e1a5bb9211227eba96ab295452d1cb21
Ramona <ramona.beck@bluewin.ch>
2019-05-14 09:15:44 +0200

    New files for DeepFashion training

---
+++ exemplar_cnn_dataset.py
@@ -0,0 +1,19 @@
+"""Create data set."""
+from torch.utils.data import Dataset
+
+
+class ExemplarCNNDataset(Dataset):
+    """Create a data set for the exemplar cnn task."""
+
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        data = self.data[index][0]
+        target = index
+
+        return data, target
